073d56432ffe9ed0c27f5c2920eb3c252e3f027a
Devin Walton <dmw072@latech.edu>
2024-11-05 14:37:45 -0600

    nothing here

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # Name: Devin Walton
 """
 Possible Features and Improvements:
-1. Death Room - Introduces a special room that ends the game upon entry. ###COMPLETE###
-2. Inspect Item Command - Allows players to examine items closely, uncovering details and hints. ###IN PROGRESS###
-3. Randomized Item Assignment - Places items in different rooms each playthrough for variety. ###STILL DECIDING###
-4. Randomized Traps - Randomly assigns traps to rooms, challenging players with changing obstacles. ###STILL DECIDING###
-5. Map of Explored Rooms - Shows a map that updates as players discover new rooms. ###STILL DECIDING###
-6. Status Class for Game State Management - A new Status class to manage the game's current state and handle conditions like win/loss or other game-ending scenarios. ###COMPLETE###
-7. Unlockable Keys - Introduces keys that players can collect and use to unlock locked doors or chests within the game. ###IN PROGRESS###
-8. Handle Unlock Method - Implements a method to manage the unlocking process, allowing players to interact with locks effectively.###IN PROGRESS###
+1. Death Room - Introduces a special room that ends the game upon entry. ### COMPLETE ###
+2. Status Class for Game State Management - A new Status class to manage the game's current state and handle conditions like win/loss or other game-ending scenarios. ### COMPLETE ###
+
+3. Unlockable Keys - Introduces keys that players can collect and use to unlock locked doors or chests within the game. ### IN PROGRESS ###
+4. Handle Unlock Method - Implements a method to manage the unlocking process, allowing players to interact with locks effectively.### IN PROGRESS ###
+5. Inspect Item Command - Allows players to examine items closely, uncovering details and hints. ### IN PROGRESS ###
+
+6. Randomized Item Assignment - Places items in different rooms each playthrough for variety. ### STILL DECIDING ###
+7. Randomized Traps - Randomly assigns traps to rooms, challenging players with changing obstacles. ### DECIDED AGAINST ###
+8. Map of Explored Rooms - Shows a map that updates as players discover new rooms. ### STILL DECIDING ###
+
 """
 # Date: 10/25/2024
 # Description: Room Adventure Game with a GUI
